f99dd06438b0697e1d028975e984d3932d39f248
Hai.Cao <hai.cao@ffwagency.com>
2023-08-28 18:03:15 +0700

    Update

--- wp-content/themes/vnskills/footer.php
+++ wp-content/themes/vnskills/footer.php
@@ -1,3 +1,12 @@
+<footer class="footer-wrap">
+  <?php
+    $footer_options = get_field('footer_options', 'option');
+    print('<pre>');
+    print_r($footer_options);
+    print('</pre>');
+  ?>
+</footer>
 
+<?php wp_footer(); ?>
 </body>
 </html>
